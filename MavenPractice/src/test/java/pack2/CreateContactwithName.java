4ef848151a3900cc7ada45b7276b82e7d1e7de07
gdsstsystrstrsx <reddyln123@gmail.com>
2026-09-21 15:46:13 +0530

    first message

--- MavenPractice/src/test/java/pack2/CreateContactwithName.java
+++ MavenPractice/src/test/java/pack2/CreateContactwithName.java
@@ -1,5 +1,7 @@
 package pack2;
 
 public class CreateContactwithName {
-
+public static void main(String[] args) {
+	System.out.println("dfghj");
+}
 }
--- MavenPractice/src/test/java/pack2/createcontact.java
+++ MavenPractice/src/test/java/pack2/createcontact.java
@@ -1,5 +1,7 @@
 package pack2;
 
 public class createcontact {
-
+public static void main(String[] args) {
+	System.out.println("ertyuio");
+}
 }

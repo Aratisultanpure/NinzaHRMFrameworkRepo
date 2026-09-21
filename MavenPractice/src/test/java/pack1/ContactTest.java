285e12448a5e29a21168f97cba85724333e7f00e
shiva <shiva@Shivam.mshome.net>
2026-09-21 15:03:54 +0530

    Pushing new project

---
+++ MavenPractice/src/test/java/pack1/ContactTest.java
@@ -0,0 +1,19 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("execute createContactTest");
+	}
+
+	@Test
+	public void modifyContactTest()
+	{
+		System.out.println("execute modifyContactTest");
+	}
+
+}
